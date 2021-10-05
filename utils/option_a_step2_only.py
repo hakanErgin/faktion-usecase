f4fb2e9e681d374d50846d4d4babbf29d2e0f1a9
lyesds <lrouabah@gmail.com>
2021-10-05 11:33:40 +0200

    Use of absdiff and basic classif with F1-score of 0.75 for anomalous

--- utils/option_a_step2_only.py
+++ utils/option_a_step2_only.py
@@ -1,13 +1,14 @@
 import os
 import pandas as pd
+import numpy as np
 import tabulate
 from matplotlib import pyplot as plt
+from sklearn import metrics
 from utils.visualize_substractions import visualize_substractions
 
 
-def classifier_A_step2(path):
-    df = pd.DataFrame(
-        columns=["id", "result"])
+def classifier_a_step2(path):
+    df = pd.DataFrame(columns=["id", "result"])
     ids = []
     datas = []
     for filename in os.listdir(path):
@@ -18,41 +19,50 @@ def classifier_A_step2(path):
         dfrow = [id]
         dfrow.append(res)
         df.loc[len(df)] = dfrow
-    plt.hist(df.result)
-    plt.xlim([0, 1])
-    plt.title(path)
+    # plt.hist(df.result)
+    # plt.xlim([0, 1])
+    # plt.title(path)
     # plt.show()
     return df
 
 
-df_normal = classifier_A_step2(path='./assets/normal_dice/0/')
+df_normal = classifier_a_step2(path='./assets/normal_dice/0/')
 for i in range(1, 11):
-    df_normal = pd.DataFrame.append(df_normal, classifier_A_step2(path='./assets/normal_dice/'+str(i)+'/'))
+    df_normal = pd.DataFrame.append(df_normal, classifier_a_step2(path='./assets/normal_dice/'+str(i)+'/'))
 
+df_normal['y_true'] = 0
+df_normal['y_pred'] = np.where(df_normal['result'] < 0.04, 0, 1)
 
-print(df_normal.shape)
+'''print(df_normal.shape)
 print(df_normal.describe())
 plt.hist(df_normal.result)
-plt.xlim([0, 0.1])
+#plt.xlim([0, 0.1])
 plt.title('Minimum sum of pixel values - Normal dice only')
-plt.show()
+plt.show()'''
 
 
-#df = classifier_A_step2(path='./assets/normal_dice/2/')
-#df = classifier_A_step2(path='./assets/normal_dice/3/')
-# print(df.head().to_markdown())
-# df.to_csv('./assets/df_classifier_A_step2.csv', sep=',', index=False, mode='w')
+# df.to_csv('./assets/df_classifier_a_step2.csv', sep=',', index=False, mode='w')
 
-df_anomalous = classifier_A_step2(path='./assets/anomalous_dice/')
-print(df_anomalous.describe())
+df_anomalous = classifier_a_step2(path='./assets/anomalous_dice/')
+df_anomalous['y_true'] = 1
+df_anomalous['y_pred'] = np.where(df_anomalous['result'] < 0.04, 0, 1)
+
+'''print(df_anomalous.describe())
 
 import math
 w = 0.02
 n = math.ceil((df_anomalous['result'].max() - df_anomalous['result'].min())/w)
 
 plt.hist(df_anomalous.result, bins=n)
-plt.xlim([0, 0.1])
+#plt.xlim([0, 0.1])
 plt.title('Minimum sum of pixel values - Anomalous dice only')
-plt.show()
+plt.show()'''
 
-print(df_anomalous.head(10).to_markdown())
+df_global = pd.concat([df_normal, df_anomalous])
+# print(df_global.shape)
+# print(df_global.head(10).to_markdown())
+
+report = metrics.classification_report(df_global['y_true'], df_global['y_pred'])
+confus_matrix = metrics.confusion_matrix(df_global['y_true'], df_global['y_pred'])
+print(report)
+print(confus_matrix)
